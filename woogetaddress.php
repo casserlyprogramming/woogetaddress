8b687e91e979445a47f3194701582188a3225b67
Daniel Casserly <daniel@casserlyprogramming.com>
2017-08-01 23:40:47 +0100

    Added the first file

---
+++ woogetaddress.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  Plugin Name: WooGetAddress
+  Plugin URI: http://www.casserlyprogramming.com
+  Description: Woocommerce Extension for getaddress.io 
+  Version: 1.0.0
+  Author: Daniel Casserly
+  Author URI: http://dandalfprogramming.blogspot.co.uk/
+ */
+
+
+if (!defined('ABSPATH'))
+    exit; // Exit if accessed directly
+
+
+function wga_add_address_lookup() {
+
+}
