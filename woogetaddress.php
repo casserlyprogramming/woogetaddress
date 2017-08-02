56c844be552a1d9974c379a81024e3bd23875ad3
Daniel Casserly <daniel@casserlyprogramming.com>
2017-08-02 22:35:05 +0100

    Started adding in the typeahead part of the code

--- woogetaddress.php
+++ woogetaddress.php
@@ -14,5 +14,28 @@ if (!defined('ABSPATH'))
 
 
 function wga_add_address_lookup() {
+?>
+    <input type="text" class="typeahead" placeholder="Search an address" 
+         name="billingSearch" id="billingSearch"/>
 
+    <script type="text/javascript">
+    jQuery("#billingSearch").typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 3
+    },
+    {
+        name: 'billing',
+        source: getAddressFunc()
+});
+
+function getAddressFunc() {
+    return function findMatches(q, cb) {
+        var mathes = [];
+
+        
+    }
+}
+    </script>
+<?php  
 }
